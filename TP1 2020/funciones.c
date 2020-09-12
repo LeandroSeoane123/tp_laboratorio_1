a4c39a8491b31a57081c9802e912b81f49425fe8
LeandroSeoane123 <52429721+LeandroSeoane123@users.noreply.github.com>
2020-09-12 02:16:55 +0200

    Delete funciones.c

--- TP1 2020/funciones.c
+++
@@ -1,8 +0,0 @@
-int suma(int numero1, int numero2);
-int resta(int numero1, int numero2);
-float division(float numero1, float numero2);
-int multiplicacion(int numero1, int numero2);
-int factorizacion(int numero1);
-void mostrarResultados(int respuestaSuma, int respuestaResta, float respuestaDivision, int respuestaMultiplicacion, int respuestaFactorialUno, int respuestaFactorialDos);
-
-
