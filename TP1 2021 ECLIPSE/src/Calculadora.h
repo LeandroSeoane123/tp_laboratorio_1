0cd5a44b70abba62ecab4ce2176b345a39e01f44
LeandroSeoane123 <52429721+LeandroSeoane123@users.noreply.github.com>
2021-04-14 00:43:37 -0300

    Add files via upload

---
+++ TP1 2021 ECLIPSE/src/Calculadora.h
@@ -0,0 +1,13 @@
+
+int suma(int numero1, int numero2);
+int resta(int numero1, int numero2);
+float division(float numero1, float numero2);
+int multiplicacion(int numero1, int numero2);
+int factorizacion(int numero1);
+void mostrarSuma(int respuestaSuma, int validacionSuma);
+void mostrarResta(int respuestaResta, int validacionResta);
+void mostrarDivision(float respuestaDivision, int validacionDivision);
+void mostrarMultiplicacion(int respuestaMultiplicacion, int validacionMultiplicacion);
+void mostrarFactorialUno(int respuestaFactorialUno, int validacionFactorialUno);
+void mostrarFactorialUno(int respuestaFactorialDos, int validacionFactorialDos);
+void mostrarResultados(int respuestaSuma, int validacionSuma, int respuestaResta, int validacionResta, float respuestaDivision, int validacionDivision, int respuestaMultiplicacion, int validacionMultiplicacion, int respuestaFactorialUno, int validacionFactorialUno, int respuestaFactorialDos, int validacionFactorialDos);
