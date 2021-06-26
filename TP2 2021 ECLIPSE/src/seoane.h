b0a0588bfeb99e0a34bb3fc38018e580198be1f8
LeandroSeoane123 <52429721+LeandroSeoane123@users.noreply.github.com>
2021-06-26 15:41:40 -0300

    Delete TP2 2021 ECLIPSE directory

--- TP2 2021 ECLIPSE/src/seoane.h
+++
@@ -1,8 +0,0 @@
-
-void getInt (char* mensaje, int* entero);
-void getFloat (char* mensaje, float* flotante);
-void getString (char* mensaje, char* cadena);
-void getChar (char* mensaje, char* caracter);
-void validarMenu (int opcion, int opcionMenor, int opcionMayor);
-void limpiarPantalla ();
-
